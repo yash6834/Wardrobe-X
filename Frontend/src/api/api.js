c73144ad05b6b01b5862ec8000c1d70f637968a9
Yash Patel <yashpatel7415@gmail.com>
2026-09-11 10:09:47 +0530

    Connect frontend to deployed backend

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://wardrobe-x.onrender.com",
 });
 
 /*  Attach token (admin OR user/vendor) */
